ba8937e892840befe6b41f3fb11f769c86db891e
Jesper321 <drentj7@gmail.com>
2024-10-12 20:49:01 +0200

    collapse content1 start

---
+++ Onscreen/js/icon_change.js
@@ -0,0 +1,8 @@
+function collapsibleContent1(x) {
+  const content = document.getElementById("collapsible_content");
+  content.classList.toggle("visible"); // Toggle visibility
+  x.classList.toggle("i"); 
+  x.classList.toggle("bx");
+  x.classList.toggle("bx-align-middle");
+}
+
