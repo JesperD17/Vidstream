7d7b0f74601b47fd3bd1b1204d3ded47ee3d4f16
Jesper321 <drentj7@gmail.com>
2024-10-14 20:38:55 +0200

    save

--- Onscreen/js/icon_change.js
+++ Onscreen/js/icon_change.js
@@ -1,24 +1,24 @@
-function collapsibleContent1(x) { // icon switch
-  const content = document.getElementById("collapsible_content");
-  content.classList.toggle("visible"); // visibility
-  x.classList.toggle("i"); 
-  x.classList.toggle("bx");
-  x.classList.toggle("bx-align-middle");
+// function collapsibleContent1(x) { // icon switch
+//   const content = document.getElementById("collapsible_content");
+//   content.classList.toggle("visible"); // visibility
+//   x.classList.toggle("i"); 
+//   x.classList.toggle("bx");
+//   x.classList.toggle("bx-align-middle");
 
-const links = document.querySelectorAll(".links a");
-let input = document.querySelector('i.bx.bx-align-right'); 
-let clickCounter = 0;
+// const links = document.querySelectorAll(".links a");
+// let input = document.querySelector('i.bx.bx-align-right'); 
+// let clickCounter = 0;
 
-  links.forEach(function(div) { // voor elke class die .links a heeft
-      div.style.cssText = 'display:block !important';
+//   links.forEach(function(div) { // voor elke class die .links a heeft
+//       div.style.cssText = 'display:block !important';
       
-      input.addEventListener("click", function(div) {
-        clickCounter++;
-        if (clickCounter == 1) { // voor elke keer als er een 2e keer op de icon word geklikt
-          div.style.cssText = 'display:none !important';
-        }
-      })
-  });
+//       input.addEventListener("click", function(div) {
+//         clickCounter++;
+//         if (clickCounter == 1) { // voor elke keer als er een 2e keer op de icon word geklikt
+//           div.style.cssText = 'display:none !important';
+//         }
+//       })
+//   });
 
   
 
@@ -36,4 +36,4 @@ let clickCounter = 0;
   //       });
   //     }
   //   });
-}
+// }
