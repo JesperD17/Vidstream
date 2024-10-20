2a799ae80900c77eadcef1433d8bde30ab3d188d
Jesper321 <drentj7@gmail.com>
2024-10-20 14:01:37 +0200

    finished header

--- Onscreen/js/collapsibleContent.js
+++ Onscreen/js/collapsibleContent.js
@@ -1,35 +1,55 @@
-var links = document.querySelector(".search");
-var content = document.querySelector(".content")
+var links = document.querySelector("#search");
+
+var content = document.querySelector("#content");
+
 let clickCounter = 0;
-    let input = links.querySelector('.icon'); 
-    let div = links.querySelector('.search-bar'); 
-    let input2 = content.querySelector('.icon')
-    let div2 = content.querySelector(".collapsible-bar")
+    let input = links.querySelector('i')
+    let div = links.querySelector('.search-field')
 
-    input.addEventListener("click", function() {
-        div.classList.toggle('hiden')
-        if(input.classList.contains('bx-search')) {
-            input.classList.remove('bx-search')
-            input.classList.add('bxs-search')
+    let input2 = content.querySelector('i')
+    let div2 = content.querySelector('i');
+ 
+        input.addEventListener("click", function() {
+            div.classList.toggle('hiden')
+            if(input.classList.contains('bx-search')) {
+                input.classList.remove('bx-search')
+                input.classList.add('bxs-search')
+                document.getElementById('search-field').style.display = "flex";
+                document.getElementById('search-field').style.border = "solid white 3px";
+            }
+            else {
+                input.classList.remove('bxs-search')
+                input.classList.add('bx-search')
+                document.getElementById('search-field').style.display = "none";
+                document.getElementById('search-field').style.border = "none";
+            }
+        })
 
-        }
-        else {
-            input.classList.remove('bxs-search')
-            input.classList.add('bx-search')
-        }
-    })
 
-    // input2.addEventListener("click", function() {
-    //     div2.classList.toggle('hiden')
-    //     if(input2.classList.contains('bx-align-right')) {
-    //         input2.classList.remove('bx-align-right')
-    //         input2.classList.add('bx-align-middle')
-    //     }
-    //     else {
-    //         input2.classList.remove('bx-align-middle')
-    //         input2.classList.add('bx-align-right')
-    //     }
-    // })
+
+        input2.addEventListener("click", function() {
+            div2.classList.toggle('hiden')
+            if(input2.classList.contains('bx-align-left')) {
+                input2.classList.remove('bx-align-left')
+                input2.classList.add('bx-align-middle')
+
+
+                document.querySelectorAll('.links a').forEach(link => {
+                    link.style.display = "block"; 
+                });
+                // document.getElementById('search-field').style.border = "solid white 3px";
+            }
+            else {
+                input2.classList.remove('bx-align-middle')
+                input2.classList.add('bx-align-left')
+
+                document.querySelectorAll('.links a').forEach(link => {
+                    link.style.display = "none"; 
+                });
+                // document.getElementById('search-field').style.display = "none";
+                // document.getElementById('search-field').style.border = "none";
+            }
+        })
 
 
 
