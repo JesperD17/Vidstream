c90e9b3665295dc5c6fc0c33721fc621e65cfe53
Jesper <jesper@nedfinity.com>
2024-10-21 09:48:18 +0200

    spotlight movies start, card styles and onscreen

--- Onscreen/js/load_in_API.js
+++ Onscreen/js/load_in_API.js
@@ -17,25 +17,27 @@
     fStart();
     
     function fShow(movies) {
-        let html = "<h3>Hieronder komen de movies</h3>";
+        let html = "";
     
         movies.forEach(function (movie, index) {
-            // Create a unique class for each movie, e.g., "movie-banner-1"
-            const cardClass = `Card-${index + 1}`;
-            const bannerClass = `movie-banner-${index + 1}`;
-    
+            const cardClass = `Card ${index + 1}`;
+            const bannerClass = `movie-banner ${index + 1}`;
+                // <img src="${movie.banner}" alt="${movie.title} banner"/>
             html += `
             <div class="${cardClass}">
                 <div class="${bannerClass}">
-                    <img src="${movie.banner}" alt="${movie.title} banner"/>
+                    <img src="${movie.poster}" alt="${movie.title} poster"/>
                 </div>
-                <div class="titles">${index + 1}. ${movie.title}</div>
-                <div></div>
+                <div class="card_info">
+                    <div class="titles">${index + 1}. ${movie.title}</div>
+                    <div class="release_date">${movie.year}</div>
+                    <div class="ratings">${movie.rating}</div>
+                </div>
             </div>
             `;
     
             console.log(`Movie: ${movie.title}, Banner Class: ${bannerClass}`);
         });
     
-        document.querySelector(".output").innerHTML += html;
+        document.querySelector(".Spotlight").innerHTML += html;
     }
