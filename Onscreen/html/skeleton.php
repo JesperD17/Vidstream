a39a359baa3ca7a568e5b2c9774f61283d92930b
Jesper <jesper@nedfinity.com>
2024-11-04 13:02:59 +0100

    footer finishing touches, skeleton busy.

---
+++ Onscreen/html/skeleton.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/skeleton_homepage.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/homepage.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <title class="loader"></title>
+</head>
+<body>
+    <div id="shell">
+    <?php include 'header.html' ?>
+
+        <div id="main"></div>
+        
+        <?php include 'footer.html' ?>
+    </div>
+</body>
+</html>
+
+<script src="../js/html_Optamized.js"></script>
+<script src="../js/loading.js"></script>
---
+++ Onscreen/js/loading.js
@@ -0,0 +1,10 @@
+// history.pushState(null, null, location.href);
+
+// var dots = window.setInterval( function() {
+//     var wait = document.querySelector(".loading");
+//     if ( wait.innerHTML.length > 3 ) 
+//         wait.innerHTML = "";
+//     else 
+//         wait.innerHTML += ".";
+//     }, 500);
+
